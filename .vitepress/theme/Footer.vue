52df2d492e33880e9341b195dfb003a2f7cddf0a
Bijit Mondal <bijitmondal3011@rediffmail.com>
2025-04-19 11:38:09 +0530

    feat(ui): footer and navbar done

---
+++ .vitepress/theme/Footer.vue
@@ -0,0 +1,117 @@
+<template>
+    <footer>
+      <div class="icons">
+        <div class="no-wrap-icons">
+          <a href="#" data-splitbee-event="External Link" data-splitbee-event-destination="github" target="_blank" rel="noopener noreferrer">
+            <!-- Github -->
+            <svg t="1625711611350" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2770"
+                 xmlns:xlink="http://www.w3.org/1999/xlink" width="200" height="200" class="vp-icon">
+              <defs>
+              </defs>
+              <path
+                  d="M64.6 512c0 195.6 125.4 361.9 300.1 422.9 23.5 5.9 19.9-10.8 19.9-22.2v-77.6c-135.8 15.9-141.3-74-150.5-89-18.5-31.5-61.9-39.5-49-54.5 31-15.9 62.5 4 98.9 58 26.4 39.1 77.9 32.5 104.1 26 5.7-23.5 17.9-44.5 34.7-60.9-140.7-25.2-199.4-111.1-199.4-213.3 0-49.5 16.4-95.1 48.4-131.8-20.4-60.6 1.9-112.4 4.9-120.1 58.2-5.2 118.5 41.6 123.3 45.3 33.1-8.9 70.8-13.7 112.9-13.7 42.4 0 80.3 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.4-43.9 2.9 7.7 24.7 58.3 5.5 118.1 32.5 36.8 49 82.8 49 132.4 0 102.3-59 188.3-200.2 213.2 23.5 23.3 38.1 55.5 38.1 91.1v112.7c0.8 9 0 17.9 15.1 17.9C832.7 877 960.4 709.4 960.4 512.1c0-247.5-200.6-447.9-447.9-447.9C265 64.1 64.6 264.5 64.6 512z"
+                  p-id="2771"></path>
+            </svg>
+          </a>
+  
+          <a data-splitbee-event="External Link" data-splitbee-event-destination="twitter" href="#" target="_blank" rel="noopener noreferrer">
+            <!-- X -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="vp-icon" viewBox="0 0 24 24">
+              <g fill="none" stroke="#8B70CD" stroke-width="1.5">
+                <path
+                    d="M16.82 20.768L3.753 3.968A.6.6 0 0 1 4.227 3h2.48a.6.6 0 0 1 .473.232l13.067 16.8a.6.6 0 0 1-.474.968h-2.48a.6.6 0 0 1-.473-.232Z" />
+                <path stroke-linecap="round" d="M20 3L4 21" />
+              </g>
+            </svg>
+          </a>
+          <a href="#" data-splitbee-event="External Link" data-splitbee-event-destination="discord" target="_blank" rel="noopener noreferrer">
+            <!-- Discord -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="vp-icon" viewBox="0 0 24 24">
+              <g fill="none" stroke="#8B70CD" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
+                <path d="M5.5 16c5 2.5 8 2.5 13 0"></path>
+                <path d="m15.5 17.5l1 2s4.171-1.328 5.5-3.5c0-1 .53-8.147-3-10.5c-1.5-1-4-1.5-4-1.5l-1 2h-2"></path>
+                <path d="m8.528 17.5l-1 2s-4.171-1.328-5.5-3.5c0-1-.53-8.147 3-10.5c1.5-1 4-1.5 4-1.5l1 2h2"></path>
+                <path
+                    d="M8.5 14c-.828 0-1.5-.895-1.5-2s.672-2 1.5-2s1.5.895 1.5 2s-.672 2-1.5 2m7 0c-.828 0-1.5-.895-1.5-2s.672-2 1.5-2s1.5.895 1.5 2s-.672 2-1.5 2">
+                </path>
+              </g>
+            </svg>
+          </a>
+          <a href="#" data-splitbee-event="External Link" data-splitbee-event-destination="linkedin" target="_blank" rel="noopener noreferrer">
+            <!-- LinkedIn -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="vp-icon" viewBox="0 0 24 24">
+              <g fill="none" stroke="#8B70CD" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
+                <path d="M21 8v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5M7 17v-7" />
+                <path d="M11 17v-3.25M11 10v3.75m0 0c0-3.75 6-3.75 6 0V17M7 7.01l.01-.011" />
+              </g>
+            </svg>
+          </a>
+  
+          <a href="#" data-splitbee-event="External Link" data-splitbee-event-destination="youtube" target="_blank" rel="noopener noreferrer">
+            <!-- YouTube -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="vp-icon" viewBox="0 0 32 32">
+              <path
+                  d="M16 6c-3.766 0-7.094.39-9.125.688c-1.68.246-3.035 1.511-3.344 3.187C3.27 11.301 3 13.387 3 16s.27 4.7.531 6.125c.309 1.676 1.664 2.945 3.344 3.188c2.04.296 5.379.687 9.125.687c3.746 0 7.086-.39 9.125-.688c1.68-.242 3.035-1.511 3.344-3.187c.261-1.43.531-3.52.531-6.125s-.266-4.695-.531-6.125c-.309-1.676-1.664-2.941-3.344-3.188C23.094 6.391 19.765 6 16 6m0 2c3.633 0 6.879.371 8.844.656A1.966 1.966 0 0 1 26.5 10.25c.242 1.32.5 3.277.5 5.75c0 2.469-.258 4.43-.5 5.75a1.957 1.957 0 0 1-1.656 1.594C22.87 23.629 19.609 24 16 24c-3.61 0-6.875-.371-8.844-.656A1.962 1.962 0 0 1 5.5 21.75C5.258 20.43 5 18.477 5 16c0-2.48.258-4.43.5-5.75a1.962 1.962 0 0 1 1.656-1.594C9.117 8.371 12.367 8 16 8m-3 2.281V21.72l1.5-.844l7-4L23 16l-1.5-.875l-7-4zm2 3.438L18.969 16L15 18.281z" />
+            </svg>
+          </a>
+        </div>
+  
+      </div>
+      <p class="made-by">All © reserved by <a href="#">Bhupendra Yogi</a></p>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Footer',
+  };
+  </script>
+  
+  <style>
+  
+  footer {
+    padding: 1rem 0;
+    text-align: center;
+    font-family: Arial, sans-serif;
+  }
+  
+  .icons {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 15px;
+  }
+  
+  .no-wrap-icons, .wrap-icons {
+    color: transparent;
+    display: flex;
+    gap: 15px;
+  }
+  
+  .vp-icon {
+    width: 30px;
+    height: 30px;
+    transition: fill 0.3s;
+    fill: var(--vp-c-brand);
+  }
+  
+  .vp-icon:hover {
+    fill: var(--vp-c-brand-dark);
+  }
+  
+  .made-by {
+    margin: 0;
+    font-size: 14px;
+    font-family: "Space Grotesk", sans-serif;
+  }
+  
+  .made-by a {
+    color: var(--vp-c-brand-light);
+    text-decoration: none;
+    transition: color 0.3s;
+  }
+  
+  .made-by a:hover {
+    color: var(--vp-c-brand);
+  }
+  
+  </style>
